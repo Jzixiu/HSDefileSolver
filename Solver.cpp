73537333870863239b96124c33622e04334f1403
Jzixiu <78724189+Jzixiu@users.noreply.github.com>
2025-03-28 02:51:02 -0400

    实现了Solver类

---
+++ Solver.cpp
@@ -0,0 +1,95 @@
+#include "Solver.h"
+
+Solver::Solver(GameState &game)
+    : gs(game), solution_atk(-1), elapsed_time(-1LL),
+      incumbent_callback([](int, const std::vector<step> &) {})
+{
+}
+
+void Solver::solve()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    dfs();
+    auto end = std::chrono::high_resolution_clock::now();
+    elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+}
+
+void Solver::register_incumbent_callback(std::function<void(int, const std::vector<step> &)> function)
+{
+    incumbent_callback = function;
+};
+
+int Solver::get_solution_atk() const
+{
+    return solution_atk;
+}
+
+int Solver::get_elapsed_time() const
+{
+    return elapsed_time;
+}
+
+std::vector<Solver::step> Solver::get_solution() const
+{
+    return solution;
+}
+
+void Solver::attack(int ally_pos, int enemy_pos)
+{
+    step s;
+    s.ally_pos=ally_pos;
+    s.enemy_pos=enemy_pos;
+    steps.push_back(s);
+
+    gs.attack(ally_pos,enemy_pos);
+}
+
+void Solver::undo()
+{
+    steps.pop_back();
+    gs.undo();
+}
+
+bool Solver::dfs()
+{
+    if (check_solution())
+    {
+        return true;
+    }
+
+    const std::vector<int> &enemy = gs.get_enemy();
+    const std::vector<int> &ally = gs.get_ally();
+
+    for (int ally_pos : ally)
+    {
+        for (int enemy_pos : enemy)
+        {
+            attack(ally_pos, enemy_pos);
+            if (dfs())
+            {
+                undo();
+                return true;
+            }
+            undo();
+        }
+    }
+
+    return false;
+}
+
+bool Solver::check_solution()
+{
+    int atk=gs.get_enemy_atk_after_Defile();
+    if(atk<solution_atk || solution_atk==-1)
+    {
+        //有更优解
+        solution_atk=atk;
+        solution=steps;
+        if(atk==0)
+        {
+            //有最优解
+            return true;
+        }
+    }
+    return false;
+}
--- Solver.h
+++ Solver.h
@@ -1,9 +1,40 @@
 #ifndef SOLVER_H
 #define SOLVER_H
 
+#include "GameState.h"
+#include <vector>
+
 class Solver
 {
+public:
+    struct step
+    {
+        int ally_pos;
+        int enemy_pos;
+    };
 
+    Solver(GameState &game);
+    void solve();
+    void register_incumbent_callback(std::function<void(int, const std::vector<step> &)> function);
+
+    int get_solution_atk() const;
+    int get_elapsed_time() const;
+    std::vector<step> get_solution() const;
+
+private:
+    GameState &gs;
+    std::function<void(int, const std::vector<step> &)> incumbent_callback;
+
+    std::vector<step> steps;
+    int solution_atk;
+    std::vector<step> solution;
+    long long elapsed_time;
+
+    void attack(int ally_pos, int enemy_pos);
+    void undo();
+
+    bool dfs();
+    bool check_solution();
 };
 
 #endif
